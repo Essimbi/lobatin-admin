f104354af4b7fe7056e77297055662148fe180b4
Essimbi <essimbideranot@gmail.com>
2024-09-02 14:20:15 +0100

    Ajout de la box pour les stats

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,7 +5,10 @@ const Home = () => {
         <>
             <Box bg='#008582' h='100vh'>
                 <Nav/>
-                <Container maxW='container.xl'>
+                <Container maxW='container.xl' mt={4} p={4}>
+                    <Box bg="white" w='100%' h='15vh' rounded={10}>
+
+                    </Box>
                     <h1>Home page</h1>
                 </Container>
             </Box>
