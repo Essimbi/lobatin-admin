2021023ce21842e520fd2c68b6c8bc1d12b7e74a
Essimbi <essimbideranot@gmail.com>
2024-09-06 23:17:04 +0100

    Mise à jour de la gestion des utilisateurs

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -32,14 +32,23 @@ import SearchBar from "../../components/SearchBar";
 import { AddIcon, CopyIcon, DeleteIcon, EditIcon, EmailIcon, InfoOutlineIcon, PhoneIcon } from "@chakra-ui/icons";
 
 const Home = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { isOpen: isLicenceModalOpen, onOpen: onOpenLicenceModal, onClose: onCloseLicenceModal } = useDisclosure();
+    const { isOpen: isUserModalOpen, onOpen: onOpenUserModal, onClose: onCloseUserModal } = useDisclosure();
+
     const [data, setData] = useState([]);
     const [totalKey, setTotalKey] = useState(0);
+    const [totalUser, setTotalUser] = useState(0);
     const [userData, setUserData] = useState([])
     const [dateCreation, setDateCreation] = useState(new Date())
     const [dateFin, setDateFin] = useState(new Date())
     const [keyUser, setKeyUser] = useState('')
     const [isSubmitting, setIsSubmitting] = useState(false);
+    const [userName, setUserName] = useState('')
+    const [userSecName, setUserSecName] = useState('')
+    const [userPhone, setUserPhone] = useState('')
+    const [userEmail, setUserEmail] = useState('')
+    const [isUpdate, setIsUpdate] = useState(false)
+    const [userId, setUserId] = useState('')
     const token = JSON.parse(localStorage.getItem('labatin_admin_access_token'));
 
     useEffect(() => {
@@ -85,7 +94,7 @@ const Home = () => {
     
                 const result = await response.json();
                 setUserData(result.data); // Mettre à jour les données avec la réponse
-                // setTotalKey(result.data.length)
+                setTotalUser(result.data.length)
             } catch (error) {
                 console.error('Erreur lors de la récupération des données:', error);
             }
@@ -150,6 +159,7 @@ const Home = () => {
 
             const result = await response2.json();
             setUserData(result.data);
+            setTotalUser(result.data.length)
         } catch (error) {
             console.error('Error:', error.message);
         }
@@ -167,6 +177,19 @@ const Home = () => {
                 });
         } else {
             console.log('Clé de licence non trouvée');
+        }
+    }
+
+    const handleCopyUser = (rowId) => {
+        const user = userData.find(item => item._id === rowId)
+        if (user) {
+            setIsUpdate(true)
+            setUserName(user.name)
+            setUserSecName(user.secname)
+            setUserPhone(user.phoneNumber)
+            setUserEmail(user.email)
+            setUserId(rowId)
+            onOpenUserModal();
         }
     }
 
@@ -250,7 +273,7 @@ const Home = () => {
                     <IconButton
                     aria-label='Copier la clé'
                     size='sm'
-                    onClick={() => handleCopyKey(row._id)}
+                    onClick={() => handleCopyUser(row._id)}
                     icon={<EditIcon />}
                     title="Copier la clé"
                     /> &nbsp;&nbsp;
@@ -351,7 +374,7 @@ const Home = () => {
             }
 
             const result = await response2.json();
-            setData(result.data); // Mettre à jour les données avec la réponse
+            setData(result.data);
             setTotalKey(result.data.length)
         } catch (error) {
             console.error('Error:', error.message);
@@ -360,7 +383,102 @@ const Home = () => {
             setKeyUser('');
             setDateCreation(new Date());
             setDateFin(new Date());
-            onClose(); // Fermer le modal après la soumission
+            onCloseLicenceModal();
+        }
+    }
+
+    const onSubmitSaveUser = async (e) => {
+        e.preventDefault();
+        const bodyRequest = {
+            name: userName,
+            secname: userSecName,
+            phoneNumber: userPhone,
+            email: userEmail
+        }
+        setIsSubmitting(true);
+        try {
+            const response = await fetch('https://lobatin-api.vercel.app/user/create', {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
+              body: JSON.stringify(bodyRequest),
+            });
+            
+            if (response.success === false) {
+                throw new Error("Erreur de génération de clé");
+            }
+            
+            const response2 = await fetch('https://lobatin-api.vercel.app/user/', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}` // Ajouter le token dans les headers
+                }
+            });
+            
+            if (response2.success === false) {
+                throw new Error('Erreur lors de la requête');
+            }
+
+            const result = await response2.json();
+            setUserData(result.data);
+            setTotalUser(result.data.length)
+        } catch (error) {
+            console.error('Error:', error.message);
+        } finally {
+            setIsSubmitting(false);
+            setUserName('')
+            setUserSecName('')
+            setUserPhone('')
+            setUserEmail('')
+            onCloseUserModal();
+        }
+    }
+
+    const onSubmitUpdateUser = async (e) => {
+        e.preventDefault();
+        const bodyRequest = {
+            name: userName,
+            secname: userSecName,
+            phoneNumber: userPhone,
+            email: userEmail
+        }
+        setIsSubmitting(true);
+        try {
+            const response = await fetch(`https://lobatin-api.vercel.app/user/update/${userId}`, {
+              method: 'PATCH',
+              headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
+              body: JSON.stringify(bodyRequest),
+            });
+            
+            if (response.success === false) {
+                throw new Error("Erreur de génération de clé");
+            }
+            
+            const response2 = await fetch('https://lobatin-api.vercel.app/user/', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}` // Ajouter le token dans les headers
+                }
+            });
+            
+            if (response2.success === false) {
+                throw new Error('Erreur lors de la requête');
+            }
+
+            const result = await response2.json();
+            setUserData(result.data);
+        } catch (error) {
+            console.error('Error:', error.message);
+        } finally {
+            setIsSubmitting(false);
+            setUserName('')
+            setUserSecName('')
+            setUserPhone('')
+            setUserEmail('')
+            setUserId('')
+            setIsUpdate(false)
+            onCloseUserModal();
         }
     }
     
@@ -373,7 +491,7 @@ const Home = () => {
                     <Box bg="white" w='100%' rounded={10} p={4}>
                         <Wrap spacing='30px' justify='center'>
                             <WrapItem>
-                                <TotalUser />
+                                <TotalUser totalUser={totalUser} />
                             </WrapItem>
                             <WrapItem>
                                 <TotalD />
@@ -394,7 +512,7 @@ const Home = () => {
                                 <TabPanel>
                                     <Box>
                                         <SearchBar onSearch={setSearchTerm} />
-                                        <Button leftIcon={<AddIcon />} colorScheme='cyan' variant='solid' ml={4} style={{ color: 'white' }} onClick={onOpen}>
+                                        <Button leftIcon={<AddIcon />} colorScheme='cyan' variant='solid' ml={4} style={{ color: 'white' }} onClick={onOpenLicenceModal}>
                                             Nouvelle licence
                                         </Button>
                                         <DataTable
@@ -412,7 +530,7 @@ const Home = () => {
                                 <TabPanel>
                                     <Box>
                                         <SearchBar onSearch={setSearchUser} />
-                                        <Button leftIcon={<AddIcon />} colorScheme='cyan' variant='solid' ml={4} style={{ color: 'white' }} onClick={onOpen}>
+                                        <Button leftIcon={<AddIcon />} colorScheme='cyan' variant='solid' ml={4} style={{ color: 'white' }} onClick={onOpenUserModal}>
                                             Nouvel utilisateur
                                         </Button>
                                         <DataTable
@@ -433,7 +551,7 @@ const Home = () => {
                 </Container>
             </Box>
 
-            <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose} isCentered>
+            <Modal blockScrollOnMount={false} isOpen={isLicenceModalOpen} onClose={onCloseLicenceModal} isCentered>
                 <ModalOverlay />
                 <ModalContent>
                     <ModalHeader>Générer une nouvelle clé </ModalHeader>
@@ -464,7 +582,7 @@ const Home = () => {
                         </ModalBody>
 
                         <ModalFooter>
-                            <Button mr={3} onClick={onClose} type="reset">
+                            <Button mr={3} onClick={onCloseLicenceModal} type="reset">
                                 Annuler
                             </Button>
                             <Button 
@@ -481,54 +599,54 @@ const Home = () => {
                 </ModalContent>
             </Modal>
 
-            <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose} isCentered>
+            <Modal blockScrollOnMount={false} isOpen={isUserModalOpen} onClose={onCloseUserModal} isCentered>
                 <ModalOverlay />
                 <ModalContent>
                     <ModalHeader>Enrégistrer un utilisateur </ModalHeader>
                     <ModalCloseButton />
-                    <form>
+                    <form onSubmit={isUpdate ? onSubmitUpdateUser : onSubmitSaveUser}>
                         <ModalBody>
                             {/* <Autocomplete options={options} /> */}
                             <FormControl marginBottom={2} >
                                 <FormLabel>Nom</FormLabel>
-                                <InputGroup size='sm'>
+                                <InputGroup>
                                     <InputRightElement>
                                         <InfoOutlineIcon color='gray.300' />
                                     </InputRightElement>
-                                    <Input type='text' sx={{ '&:focus': { borderColor: '#008582', boxShadow: '0 0 0 1px teal.500',},}}/>
+                                    <Input type='text' value={userName} required sx={{ '&:focus': { borderColor: '#008582', boxShadow: '0 0 0 1px teal.500',},}} onChange={(e) => { setUserName(e.target.value) }}/>
                                 </InputGroup>
                             </FormControl>
                             <FormControl marginBottom={2} >
                                 <FormLabel>Prénom</FormLabel>
-                                <InputGroup size='sm'>
+                                <InputGroup>
                                     <InputRightElement>
                                         <InfoOutlineIcon color='gray.300' />
                                     </InputRightElement>
-                                    <Input type='text' sx={{ '&:focus': { borderColor: '#008582', boxShadow: '0 0 0 1px teal.500',},}}/>
+                                    <Input type='text' value={userSecName} required sx={{ '&:focus': { borderColor: '#008582', boxShadow: '0 0 0 1px teal.500',},}} onChange={(e) => { setUserSecName(e.target.value) }}/>
                                 </InputGroup>
                             </FormControl>
                             <FormControl marginBottom={2}>
                                 <FormLabel>Téléphone</FormLabel>
-                                <InputGroup size='sm'>
+                                <InputGroup>
                                     <InputRightElement>
                                         <PhoneIcon color='gray.300' />
                                     </InputRightElement>
-                                    <Input type='tel' sx={{ '&:focus': { borderColor: '#008582', boxShadow: '0 0 0 1px teal.500',},}}/>
+                                    <Input type='tel' value={userPhone} required sx={{ '&:focus': { borderColor: '#008582', boxShadow: '0 0 0 1px teal.500',},}} onChange={(e) => { setUserPhone(e.target.value) }}/>
                                 </InputGroup>
                             </FormControl>
                             <FormControl mb={2}>
                                 <FormLabel>Adresse mail</FormLabel>
-                                <InputGroup size='sm'>
+                                <InputGroup>
                                     <InputRightElement>
                                         <EmailIcon color='gray.300' />
                                     </InputRightElement>
-                                    <Input type='email' sx={{ '&:focus': { borderColor: '#008582', boxShadow: '0 0 0 1px teal.500',},}}/>
+                                    <Input type='email' value={userEmail} required sx={{ '&:focus': { borderColor: '#008582', boxShadow: '0 0 0 1px teal.500',},}} onChange={(e) => { setUserEmail(e.target.value) }}/>
                                 </InputGroup>
                             </FormControl>
                         </ModalBody>
 
                         <ModalFooter>
-                            <Button mr={3} onClick={onClose} type="reset">
+                            <Button mr={3} onClick={onCloseUserModal} type="reset">
                                 Annuler
                             </Button>
                             <Button 
@@ -536,9 +654,9 @@ const Home = () => {
                                 colorScheme="teal" 
                                 type="submit"
                                 isLoading={isSubmitting}
-                                loadingText='Générer'
+                                loadingText={isUpdate ? 'Mettre à jour' : 'Enregistrer'}
                             >
-                                Générer
+                                {isUpdate ? 'Mettre à jour' : 'Enregistrer'}
                             </Button>
                         </ModalFooter>
                     </form>
--- src/components/TotalUser/index.jsx
+++ src/components/TotalUser/index.jsx
@@ -1,5 +1,5 @@
 import { Card, CardBody } from '@chakra-ui/react'
-const TotalUser = () => {
+const TotalUser = ({totalUser}) => {
   return (
     <>
       <Card
@@ -10,6 +10,7 @@ const TotalUser = () => {
       >
         <CardBody>
             <h4><b>Total d'utilisateur de l'application</b></h4>
+            <p style={{ textAlign: 'center', marginTop: '1vh', fontSize: '34px' }}> <b> {totalUser} </b> </p>
         </CardBody>
       </Card>
     </>
